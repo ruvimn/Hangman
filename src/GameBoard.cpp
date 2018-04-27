97d4b76e71a4c74f1960a21369976976b96bc566
ruvimn <nochvayr@gmail.com>
2018-04-27 15:41:17 -0500

    Draw board method complete
    issue #4

--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -2,8 +2,75 @@
 #include <iostream>
 using namespace std;
 
-GameBoard::GameBoard() {
-
+GameBoard::GameBoard()
+{
+    switch(missed_guesses)
+    {
+        case 0:
+            cout << "   /------\ " << endl;
+            cout << "   |      | " << endl;
+            cout << "   |        " << endl;
+            cout << "   |        " << endl;
+            cout << "   |        " << endl;
+            cout << "   |        " << endl;
+            cout << "   \-------|" << endl;
+        case 1:
+            cout << "   /------\ " << endl;
+            cout << "   |      | " << endl;
+            cout << "   |      0 " << endl;
+            cout << "   |        " << endl;
+            cout << "   |        " << endl;
+            cout << "   |        " << endl;
+            cout << "   \-------|" << endl;
+        case 2:
+            cout << "   /------\ " << endl;
+            cout << "   |      | " << endl;
+            cout << "   |      0 " << endl;
+            cout << "   |      | " << endl;
+            cout << "   |        " << endl;
+            cout << "   |        " << endl;
+            cout << "   \-------|" << endl;
+        case 3:
+            cout << "   /------\  " << endl;
+            cout << "   |      |  " << endl;
+            cout << "   |      0  " << endl;
+            cout << "   |     \|  " << endl;
+            cout << "   |         " << endl;
+            cout << "   |         " << endl;
+            cout << "   \-------| " << endl;
+        case 4:
+            cout << "   /------\  " << endl;
+            cout << "   |      |  " << endl;
+            cout << "   |      0  " << endl;
+            cout << "   |     \|/ " << endl;
+            cout << "   |         " << endl;
+            cout << "   |         " << endl;
+            cout << "   \-------| " << endl;
+        case 5:
+            cout << "   /------\  " << endl;
+            cout << "   |      |  " << endl;
+            cout << "   |      0  " << endl;
+            cout << "   |     \|/ " << endl;
+            cout << "   |     /   " << endl;
+            cout << "   |         " << endl;
+            cout << "   \-------| " << endl;
+        case 6:
+            cout << "   /------\  " << endl;
+            cout << "   |      |  " << endl;
+            cout << "   |      0  " << endl;
+            cout << "   |     \|/ " << endl;
+            cout << "   |     / \ " << endl;
+            cout << "   |         " << endl;
+            cout << "   \-------| " << endl;
+        default:
+            cout << "   /------\  " << endl;
+            cout << "   |      |  " << endl;
+            cout << "   |      0  " << endl;
+            cout << "   |     \|/ " << endl;
+            cout << "   |     / \ " << endl;
+            cout << "   |         " << endl;
+            cout << "   \-------| " << endl;
+    }
 }
 
 void GameBoard::draw_board(void) {
